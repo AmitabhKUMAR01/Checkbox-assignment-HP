ab6512eef44c7f959e7e364303f7d5a4d7659267
AmitabhKUMAR01 <amitabhkumar7668@gmail.com>
2024-08-21 18:15:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+ $(document).ready(function() {
+            $('.checkbox-wrapper').slideDown();
+            $('.module-checkbox').change(function() {
+                $(this).closest('.module-section').find('.option-checkbox').prop('checked', this.checked);
+            });
+          
+            $('.option-checkbox').change(function() {
+                const section = $(this).closest('.module-section');
+                const allChecked = section.find('.option-checkbox:checked').length === section.find('.option-checkbox').length;
+                const anyChecked = section.find('.option-checkbox:checked').length > 0;
+                section.find('.module-checkbox').prop('checked', allChecked || anyChecked);
+                console.log("section",section)
+                console.log("allchecked",allChecked)
+                console.log("anychecked",anyChecked)
+                console.log(allChecked||anyChecked)
+                console.log( section.find('.option-checkbox:checked'))
+            });
+        });
